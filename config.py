40570ec6f4be925aff929855f3474dce96fa4a6a
Tegar Prayuda <115115864+GarzProject@users.noreply.github.com>
2022-10-25 19:57:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
 import dotenv
+
+
+# VARIABLE
+
+
+
+
+# JAWABAN BOT
+kenapa = ["gatau", "ntahlah", "tanya orang lain aja", "yntkts", "karna because tidak pernah never", "bodoamat", "apasiii wibu gaboleh nanya", "karna lu wibu", "karna aku mencintaimu", "karna kamu jelek", "pikir sendiri", "tanya google aja"]
+
+kapan = ["mungkin sekarang", "1 jam lagi", "2 jam lagi", "8 jam lagi", "hari ini", "mungkin besok", "besok lusa", "sebulan lagi", "setahun lagi", "sewindu lagi", "tahun lalu", "tahun 1945 17 agustus", "tahun 2070", "diem deh gausah nanya", "apasi wibu", "pikir sendiri", "seabad lagi", "5000 SM", "minggu ini", "tahun ini"]
+
+apakah = ["betul", "tidak", "iya", "ga", "kepo deh", "diem deh gausah nanya mulu", "bisa jadi si", "kurang tau", "bodo ah", "hmm", "ga mungkin", "kayanya iya", "mungkin aja", "mustahil", "pikir sendiri", "sudah pasti", "yoiii"]
+
